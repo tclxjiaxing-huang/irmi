50b5d7b7b55313445a3d0ab13e0328df70365a5e
hjx <291342418@qq.com>
2021-09-28 21:02:08 +0800

    添加readme文件

---
+++ README.md
@@ -0,0 +1,18 @@
+##gat
+####一个简单的git项目管理库
+
+###安装
+```shell script
+npm install wkgit -g
+```
+
+###使用
+####步骤一：配置项目目录绝对路径
+````shell script
+irmi config <projectPath>
+````
+####步骤二：执行对应的指令
+````shell script
+irmi [push|tag|show|del|customStep]
+````
+
--- src/command/tag/tag.js
+++ src/command/tag/tag.js
@@ -42,6 +42,9 @@ async function getTagParams(vList) {
     type: 'input',
     name: 'tagName',
     default: () => {
+      if (vList.length === 0) {
+        return '1.0.0';
+      }
       let lastV = vList[vList.length - 1];
       const regex = /\.(\d)\./;
       return lastV.replace(regex, (match, value) => {
@@ -86,11 +89,17 @@ async function chooseOptions() {
       name: '打标签',
       value: 'tag',
     }, {
+      name: '推送到远程仓库',
+      value: 'pushTag',
+    }, {
       name: '打标签->推送到远程仓库',
       value: 'tag-pushTag',
     }, {
       name: '删除本地标签',
       value: 'delTag',
+    }, {
+      name: '删除远程标签',
+      value: 'delOriginTag',
     }, {
       name: '删除本地标签->删除远程标签',
       value: 'delTag-delOriginTag',
@@ -99,20 +108,34 @@ async function chooseOptions() {
       value: 'showAllTag',
     }],
   }]);
+  const choices = filesData.map((file) => ({
+    name: file.name,
+    value: file.path,
+  }));
+  let { filesList } = await inquirer.prompt([{
+    type: 'checkbox',
+    name: 'filesList',
+    message: '选择应用(不选择则全部应用)',
+    choices: choices,
+  }]);
+  if (!filesList) {
+    filesList = filesData;
+  }
+  filesList = filesData.filter((file) => filesList.includes(file.path));
   if (options === 'showAllTag') {
     await showAllTag(filesData);
   } else {
     let params = [];
-    const vList = await getAlreadyTag(filesData[0].path);
+    const vList = await getAlreadyTag(filesList[0].path);
     if (options.indexOf('tag') !== -1) {
       params = await getTagParams(vList);
     }
     if (options.indexOf('del') !== -1) {
       params = await getDelParams(vList);
     }
-    for (let i = 0; i < filesData.length; i += 1) {
-      yellowBright(`***当前应用[${filesData[i].name}]***`);
-      await chooseSubOptions(filesData[i].path, options, params);
+    for (let i = 0; i < filesList.length; i += 1) {
+      yellowBright(`***当前应用[${filesList[i].name}]***`);
+      await chooseSubOptions(filesList[i].path, options, params);
     }
   }
 }
