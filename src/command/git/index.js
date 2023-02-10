f090e83da7d16d8843587a5add8afc8dcd3e2070
jiaxing <291342418@qq.com>
2023-02-10 10:13:25 +0800

    chore: 增加提示文案

--- src/command/git/index.js
+++ src/command/git/index.js
@@ -1,9 +1,10 @@
-const { execSteps} = require('../../utils');
+const { log, execSteps} = require('../../utils');
 async function git(options) {
 	const steps = Object.keys(options);
 	for (let i = 0; i < steps.length; i += 1) {
 		await execSteps(process.cwd(), steps[i]);
 	}
+	log.success("执行成功!");
 }
 
 module.exports = git;
